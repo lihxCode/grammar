d569ca55dd4aa9dad5461a1461e1bea29b1b8266
lihanxiong <delegatevip@163.com>
2020-05-28 19:47:28 +0800

    &vector

--- C++Grammar/C++Grammar/main.cpp
+++ C++Grammar/C++Grammar/main.cpp
@@ -10,22 +10,33 @@
 #include <stack>
 #include <vector>
 using namespace std;
+
+void asterCollsion(vector<int> & asteroids) {
+    for (int i = 0; i < asteroids.size(); i++) {
+        printf("%d",asteroids[i]);
+    }
+}
 int main(int argc, const char * argv[]) {
     
-    stack<int> s1;
-    stack<int> s2;
-    
-    int v = s1.top();
-    
-    vector<int> obj;
-    for (int i = 0; i < 10; i++) {
-        obj.push_back(i);
-    }
-    for (int i = 0; i < 5; i++) {
-        obj.pop_back();
-    }
-    for (int i = 0; i < obj.size(); i++) {
-        cout << obj[i] << endl;
-    }
+//    stack<int> s1;
+//    stack<int> s2;
+//    
+//    int v = s1.top();
+//    
+//    vector<int> obj;
+//    for (int i = 0; i < 10; i++) {
+//        obj.push_back(i);
+//    }
+//    for (int i = 0; i < 5; i++) {
+//        obj.pop_back();
+//    }
+//    for (int i = 0; i < obj.size(); i++) {
+//        cout << obj[i] << endl;
+//    }
+//    
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    asterCollsion(v1);
     return 0;
 }
